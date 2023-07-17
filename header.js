cbe35c9291c875deae2859087d94ad586421d0bf
Martin Lukas <martinlukas24@protonmail.com>
2023-07-18 00:41:50 +0100

    Chance hr to span in JS header

--- header.js
+++ header.js
@@ -3,33 +3,17 @@ const fragment = document.createDocumentFragment();
 const elements = [
   h1("Martin's Tech Blog", "motto"),
   div(span("Interests"), "line"),
-  p("Automation -×- Functional programming -×- Backend tech", "interests"),
-  hr(),
+  span("Automation -×- Functional programming -×- Backend tech", "interests"),
+  div("~", "line"),
 ];
 
 document.body.prepend(...elements);
 
 // ================ Helper functions ================
-
-function h1(text, className = "") {
-  return createElement("h1", className, text);
-}
-
-function hr() {
-  return createElement("hr");
-}
-
-function p(text, className = "") {
-  return createElement("p", className, text);
-}
-
-function div(element, className = "") {
-  return createElement("div", className, element);
-}
-
-function span(text, className = "") {
-  return createElement("span", className, text);
-}
+function h1(text, className = "") { return createElement("h1", className, text) }
+function p(text, className = "") { return createElement("p", className, text) }
+function div(element, className = "") { return createElement("div", className, element) }
+function span(text, className = "") { return createElement("span", className, text) }
 
 function createElement(tag, className, content) {
   const element = document.createElement(tag);
